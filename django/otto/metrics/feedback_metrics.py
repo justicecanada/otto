b1866c85a3fe8436477b4b777bbf31410e33ba00
jannable <jason@magnara.ca>
2024-09-10 15:48:04 -0400

    tagging for batch 2

--- django/otto/metrics/feedback_metrics.py
+++ django/otto/metrics/feedback_metrics.py
@@ -1,5 +1,7 @@
 from prometheus_client import Counter
 
+# AU-7: Custom metrics that can be used for generating reports on system usage and user interactions
+
 otto_feedback_submitted_total = Counter(
     "otto_feedback_submitted_total", "number of feedback submitted", labelnames=["user"]
 )
