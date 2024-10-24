10bf21d89300086f25003f40d275a4e9ddc4176d
juleskuehn <jules.kuehn@justice.gc.ca>
2024-10-24 14:47:48 +0000

    more tests; bug fixes in markdown splitter

--- django/librarian/utils/markdown_splitter.py
+++ django/librarian/utils/markdown_splitter.py
@@ -1,4 +1,5 @@
 import re
+from typing import List, Tuple
 
 import tiktoken
 from bs4 import BeautifulSoup
@@ -16,12 +17,21 @@ class MarkdownSplitter:
         self.current_headings = {i: None for i in range(1, 7)}
         self.last_table_header = None
 
-    def split_markdown(self, markdown_text: str) -> list:
+    def split_markdown(self, markdown_text: str) -> List[str]:
+        """
+        Split markdown into chunks.
+        Preserves <page_n> tags and includes heading breadcrumbs.
+        Repeats table headers when a table is split across chunks.
+        """
         split_texts = self._split_with_page_numbers(markdown_text)
         headings_added_texts = self._repeat_headings(split_texts)
         return headings_added_texts
 
-    def _split_with_page_numbers(self, markdown_text: str) -> list:
+    def _split_with_page_numbers(self, markdown_text: str) -> List[str]:
+        """
+        Splits text into chunks while preserving <page_n> tags.
+        Corrects missing or extra page tags due to splitting.
+        """
         last_page_number = None
         split_texts = []
         for t in self.splitter.split_text(markdown_text):
@@ -46,25 +56,24 @@ class MarkdownSplitter:
                 split_texts.append(closed_text)
         return split_texts
 
-    def _repeat_headings(self, split_texts: list) -> list:
-        headings_added_texts = []
-        for text in split_texts:
-            self.current_headings, min_level = self._get_all_headings(
-                text, self.current_headings
-            )
-            text = self._repeat_table_header_if_necessary(text, self.last_table_header)
-            self.last_table_header = self._get_last_table_header(
-                text, truncate_tokens=self.chunk_size - self.chunk_overlap
-            )
-            text = self._prepend_headings(
-                self.current_headings, text, to_level=min_level
-            )
-            headings_added_texts.append(text)
-        return headings_added_texts
-
     def _close_page_tags(self, html_string: str) -> str:
+        """
+        Corrects missing or extra page tags within a single chunk, e.g.
+        * chunk starts midway through a page (no <page_n> at start)
+        * chunk ends midway through a page (no </page_n> at end)
+        * chunk starts with </page_n-1> (with no content for that page)
+        * chunk ends with <page_n+1> (with no content for that page)
+        """
+        html_string = html_string.strip()
+        # Closing tag as the first line?
+        if html_string.split("\n", 1)[0].startswith("</page_"):
+            html_string = html_string.split("\n", 1)[1]
+        # Opening tag as the last line?
+        if html_string.rsplit("\n", 1)[-1].startswith("<page_"):
+            html_string = html_string.rsplit("\n", 1)[0]
         first_page_tag = re.search(r"<page_\d+>|</page_\d+>", html_string)
         if first_page_tag:
+            # Missing opening tag at start?
             if first_page_tag.group().startswith("</page_"):
                 first_page_num = int(re.search(r"\d+", first_page_tag.group()).group())
                 html_string = f"<page_{first_page_num}>\n{html_string}"
@@ -75,39 +84,103 @@ class MarkdownSplitter:
             if last_opening_tag and last_closing_tag:
                 last_opening_tag_num = int(re.search(r"\d+", last_opening_tag).group())
                 last_closing_tag_num = int(re.search(r"\d+", last_closing_tag).group())
+                if self.debug:
+                    print(
+                        f"\nLast opening tag: {last_opening_tag_num}, last closing tag: {last_closing_tag_num}"
+                    )
+                # Missing closing tag at end?
                 if last_opening_tag_num != last_closing_tag_num:
-                    html_string = f"{html_string}\n</page_{last_opening_tag_num}>"
+                    html_string = (
+                        f"{html_string.strip()}\n</page_{last_opening_tag_num}>"
+                    )
+        # Catch any additional issues with HTML tags
         soup = BeautifulSoup(html_string.strip() + "\n", "html.parser")
-        return str(soup).strip()
+        output = str(soup).strip()
+        if self.debug:
+            print(f"\nAfter _close_tags:\n---\n{output}\n---\n")
+        return output
 
-    def _get_heading(self, line: str):
+    def _get_heading(self, line: str) -> Tuple[int, str]:
+        """
+        Returns the heading level and text if the line is a heading.
+        Returns (None, None) if the line is not a heading.
+        """
         match = re.match(r"^#{1,6} ", line)
         if match:
             return len(match.group()) - 1, line[match.end() :]
         return None, None
 
-    def _set_headings(self, headings: dict, heading_level: int, heading_text: str):
+    def _set_headings(
+        self, headings: dict, heading_level: int, heading_text: str
+    ) -> None:
+        """
+        Modifies the headings dictionary in place to reflect the current heading.
+        Resets all headings below the current level.
+        """
+        if heading_level < 1 or heading_level > 6:
+            return
         headings[heading_level] = heading_text
         for i in range(heading_level + 1, 7):
             headings[i] = None
 
     def _prepend_headings(self, headings: dict, text: str, to_level=7) -> str:
+        """
+        Converts the headings dictionary into a breadcrumb trail (Heading 1 > Heading 2)
+        and prepends it to the text, wrapped in a <headings> tag.
+        Only includes headings above the specified level.
+        """
         headings2 = {k: v for k, v in headings.items() if k < to_level}
         if not any(headings2.values()):
-            return text.strip()
+            return text
         headings_str = f'{" > ".join([v for v in headings2.values() if v])}'
-        return f"<headings>{headings_str}</headings>\n{text}".strip()
+        return f"<headings>{headings_str}</headings>\n{text}"
 
-    def _get_all_headings(self, text: str, existing_headings: dict):
+    def _get_all_headings(
+        self, text: str, existing_headings: dict
+    ) -> Tuple[dict, dict, int]:
+        """
+        Extracts all markdown headings from the text.
+        Returns a tuple with:
+        * updated copy of headings dictionary (state at the end of the text),
+        * headings to prepend (usually the same as the updated headings),
+        * the minimum level of the headings to prepend
+        Note on "minimum": "# This" is level 1, "## This" is level 2, etc.
+        """
         lines = text.split("\n")
         headings = existing_headings.copy()
         min_level = 7
+        last_level = 0
+        headings_for_prepend = None
         for line in lines:
             level, heading_text = self._get_heading(line)
             if level is not None:
+                if level <= last_level and not headings_for_prepend:
+                    headings_for_prepend = existing_headings
                 self._set_headings(headings, level, heading_text)
-                min_level = min(min_level, level)
-        return headings, min_level
+                if not headings_for_prepend:
+                    min_level = min(min_level, level)
+                last_level = level
+        return (
+            headings,
+            headings_for_prepend if headings_for_prepend else headings,
+            min_level,
+        )
+
+    def _repeat_headings(self, split_texts: list) -> list:
+        headings_added_texts = []
+        for text in split_texts:
+            self.current_headings, headings_for_prepend, min_level = (
+                self._get_all_headings(text, self.current_headings)
+            )
+            text = self._repeat_table_header_if_necessary(text, self.last_table_header)
+            self.last_table_header = self._get_last_table_header(
+                text, truncate_tokens=self.chunk_size - self.chunk_overlap
+            )
+            text = self._prepend_headings(
+                headings_for_prepend, text, to_level=min_level
+            )
+            headings_added_texts.append(text)
+        return headings_added_texts
 
     def _truncate_text_to_tokens(self, text: str, max_tokens: int) -> str:
         tokenizer = tiktoken.get_encoding("cl100k_base")
--- django/tests/librarian/test_markdown_splitter.py
+++ django/tests/librarian/test_markdown_splitter.py
@@ -74,7 +74,7 @@ def test_split_with_page_numbers_no_content_between_page_tags():
 
 
 def test_split_with_page_numbers_overlap():
-    markdown_splitter = MarkdownSplitter(chunk_size=40, chunk_overlap=30, debug=True)
+    markdown_splitter = MarkdownSplitter(chunk_size=40, chunk_overlap=30)
     markdown_text = """
 <page_1>
 # I'm a heading!
@@ -86,7 +86,378 @@ Here's some more text to put it over the limit.
 """
     expected_output = [
         "<page_1>\n# I'm a heading!\n</page_1>\n<page_2>\nBlah blah blah, how about that text!\n</page_2>",
-        "<page_1>\n</page_1>\n<page_2>\nBlah blah blah, how about that text!\nHere's some more text to put it over the limit.\n</page_2>",
+        "<page_2>\nBlah blah blah, how about that text!\nHere's some more text to put it over the limit.\n</page_2>",
     ]
     result = markdown_splitter._split_with_page_numbers(markdown_text)
     assert result == expected_output
+
+
+def test_close_page_tags():
+    # Case 1: No page tags
+    markdown_splitter = MarkdownSplitter()
+    html_string = "This is some text."
+    expected_output = "This is some text."
+    result = markdown_splitter._close_page_tags(html_string)
+    assert result == expected_output
+
+    # Case 2: Starts with a closing page tag, and no other tags
+    markdown_splitter = MarkdownSplitter()
+    html_string = "</page_1>\nThis is some text."
+    expected_output = "This is some text."
+    result = markdown_splitter._close_page_tags(html_string)
+    assert result == expected_output
+
+    # Case 3: Starts with a closing page tag, followed by other pages
+    markdown_splitter = MarkdownSplitter()
+    html_string = "</page_1>\n<page_2>\nThis is some text.\n</page_2>"
+    expected_output = "<page_2>\nThis is some text.\n</page_2>"
+    result = markdown_splitter._close_page_tags(html_string)
+    assert result == expected_output
+
+    # Case 4: Starts with an opening page tag, and no other tags
+    markdown_splitter = MarkdownSplitter()
+    html_string = "<page_1>\nThis is some text."
+    expected_output = "<page_1>\nThis is some text.\n</page_1>"
+    result = markdown_splitter._close_page_tags(html_string)
+    assert result == expected_output
+
+    # Case 5: Multiple valid page tags
+    markdown_splitter = MarkdownSplitter()
+    html_string = """
+<page_1>
+This is some text.
+</page_1>
+<page_2>
+This is some more text.
+</page_2>
+"""
+    expected_output = html_string.strip()
+    result = markdown_splitter._close_page_tags(html_string)
+    assert result == expected_output
+
+    # Case 6: Multiple valid page tags, but missing closing tag
+    markdown_splitter = MarkdownSplitter()
+    html_string = """
+<page_1>
+This is some text.
+</page_1>
+<page_2>
+This is some more text.
+"""
+    expected_output = """
+<page_1>
+This is some text.
+</page_1>
+<page_2>
+This is some more text.
+</page_2>
+""".strip()
+    result = markdown_splitter._close_page_tags(html_string)
+    assert result == expected_output
+
+    # Case 7: Multiple valid page tags, but missing opening tag
+    markdown_splitter = MarkdownSplitter()
+    html_string = """
+This is some text.
+</page_1>
+<page_2>
+This is some more text.
+</page_2>
+"""
+    expected_output = """
+<page_1>
+This is some text.
+</page_1>
+<page_2>
+This is some more text.
+</page_2>
+""".strip()
+    result = markdown_splitter._close_page_tags(html_string)
+    assert result == expected_output
+
+    # Case 8: Compound issues 1
+    markdown_splitter = MarkdownSplitter()
+    html_string = """
+</page_1>
+<page_2>
+This is some text.
+</page_2>
+<page_3>
+"""
+    expected_output = """
+<page_2>
+This is some text.
+</page_2>
+""".strip()
+    result = markdown_splitter._close_page_tags(html_string)
+    assert result == expected_output
+
+    # Case 9: Compound issues 2
+    markdown_splitter = MarkdownSplitter()
+    html_string = """
+This is some text.
+</page_1>
+<page_2>
+This is some more text.
+"""
+    expected_output = """
+<page_1>
+This is some text.
+</page_1>
+<page_2>
+This is some more text.
+</page_2>
+""".strip()
+    result = markdown_splitter._close_page_tags(html_string)
+    assert result == expected_output
+
+
+def test_get_heading():
+    markdown_splitter = MarkdownSplitter()
+    line = "# I'm a heading!"
+    expected_output = (1, "I'm a heading!")
+    result = markdown_splitter._get_heading(line)
+    assert result == expected_output
+
+    markdown_splitter = MarkdownSplitter()
+    line = "I'm not a heading!"
+    expected_output = (None, None)
+    result = markdown_splitter._get_heading(line)
+    assert result == expected_output
+
+    markdown_splitter = MarkdownSplitter()
+    line = "###### I'm a heading!"
+    expected_output = (6, "I'm a heading!")
+    result = markdown_splitter._get_heading(line)
+    assert result == expected_output
+
+    markdown_splitter = MarkdownSplitter()
+    line = "######### Too many hashes!"
+    expected_output = (None, None)
+    result = markdown_splitter._get_heading(line)
+    assert result == expected_output
+
+
+def test_set_headings():
+    markdown_splitter = MarkdownSplitter()
+    headings = markdown_splitter.current_headings
+    # Add some headings
+    output = markdown_splitter._set_headings(headings, 1, "Heading 1")
+    expected_output = None
+    assert output == expected_output
+    expected_headings = {1: "Heading 1", 2: None, 3: None, 4: None, 5: None, 6: None}
+    assert markdown_splitter.current_headings == expected_headings
+
+    # Add another heading at the next level
+    markdown_splitter._set_headings(headings, 2, "Heading 2")
+    expected_headings = {
+        1: "Heading 1",
+        2: "Heading 2",
+        3: None,
+        4: None,
+        5: None,
+        6: None,
+    }
+    assert markdown_splitter.current_headings == expected_headings
+
+    # Add another heading at the same level
+    markdown_splitter._set_headings(headings, 2, "Another Heading 2")
+    expected_headings = {
+        1: "Heading 1",
+        2: "Another Heading 2",
+        3: None,
+        4: None,
+        5: None,
+        6: None,
+    }
+    assert markdown_splitter.current_headings == expected_headings
+
+    # Add another heading at a higher level
+    markdown_splitter._set_headings(headings, 1, "Another Heading 1")
+    expected_headings = {
+        1: "Another Heading 1",
+        2: None,
+        3: None,
+        4: None,
+        5: None,
+        6: None,
+    }
+    assert markdown_splitter.current_headings == expected_headings
+
+    # Add a heading, skipping a level
+    markdown_splitter._set_headings(headings, 3, "Heading 3")
+    expected_headings = {
+        1: "Another Heading 1",
+        2: None,
+        3: "Heading 3",
+        4: None,
+        5: None,
+        6: None,
+    }
+    assert markdown_splitter.current_headings == expected_headings
+
+    # Try to add a heading at an invalid level (this should do nothing)
+    markdown_splitter._set_headings(headings, 7, "Heading 7")
+    assert markdown_splitter.current_headings == expected_headings
+    markdown_splitter._set_headings(headings, 0, "Heading 0")
+    assert markdown_splitter.current_headings == expected_headings
+
+
+def test_prepend_headings():
+    markdown_splitter = MarkdownSplitter()
+    headings = {
+        1: "Heading 1",
+        2: "Heading 2",
+        3: "Heading 3",
+        4: "Heading 4",
+        5: "Heading 5",
+        6: "Heading 6",
+    }
+    text = "Some text."
+    expected_output = """
+<headings>Heading 1 > Heading 2 > Heading 3 > Heading 4 > Heading 5 > Heading 6</headings>
+Some text.
+""".strip()
+    result = markdown_splitter._prepend_headings(headings, text)
+    print(result)
+    assert result == expected_output
+
+    # Test with a lower to_level
+    expected_output = """
+<headings>Heading 1 > Heading 2 > Heading 3 > Heading 4</headings>
+Some text.
+""".strip()
+    result = markdown_splitter._prepend_headings(headings, text, to_level=5)
+    assert result == expected_output
+
+    # Test with some None headings
+    headings = {
+        1: "Heading 1",
+        2: "Heading 2",
+        3: None,
+        4: "Heading 4",
+        5: None,
+        6: None,
+    }
+    expected_output = """
+<headings>Heading 1 > Heading 2 > Heading 4</headings>
+Some text.
+""".strip()
+    result = markdown_splitter._prepend_headings(headings, text)
+    assert result == expected_output
+
+    # Test with a weird lower_to level (8)
+    expected_output = """
+<headings>Heading 1 > Heading 2 > Heading 4</headings>
+Some text.
+""".strip()
+    result = markdown_splitter._prepend_headings(headings, text, to_level=8)
+    assert result == expected_output
+
+    # Test with no headings
+    headings = {
+        1: None,
+        2: None,
+        3: None,
+        4: None,
+        5: None,
+        6: None,
+    }
+    expected_output = "Some text."
+    result = markdown_splitter._prepend_headings(headings, text)
+    assert result == expected_output
+
+
+def test_get_all_headings():
+    markdown_splitter = MarkdownSplitter()
+    text = """
+# Heading 1
+Some text.
+## Heading 2
+Some more text.
+"""
+    existing_headings = {
+        1: None,
+        2: None,
+        3: None,
+        4: None,
+        5: None,
+        6: None,
+    }
+    expected_headings = {
+        1: "Heading 1",
+        2: "Heading 2",
+        3: None,
+        4: None,
+        5: None,
+        6: None,
+    }
+    expected_min_level = 1
+    result = markdown_splitter._get_all_headings(text, existing_headings)
+    assert result == (expected_headings, expected_headings, expected_min_level)
+    # Check that the existing_headings dictionary was not modified
+    assert existing_headings == {
+        1: None,
+        2: None,
+        3: None,
+        4: None,
+        5: None,
+        6: None,
+    }
+
+    # Test with no headings
+    text = "Some text."
+    expected_headings = {
+        1: None,
+        2: None,
+        3: None,
+        4: None,
+        5: None,
+        6: None,
+    }
+    expected_min_level = 7
+    result = markdown_splitter._get_all_headings(text, existing_headings)
+    assert result == (expected_headings, expected_headings, expected_min_level)
+
+    # Test with some text that starts with higher level headings
+    # then has a lower level heading
+    existing_headings = {
+        1: "Heading 1 - existing",
+        2: "Heading 2 - existing",
+        3: None,
+        4: None,
+        5: None,
+        6: None,
+    }
+    text = """
+### Heading 3
+Some text.
+#### Heading 4
+Some more text.
+## Heading 2 - new
+Even more text.
+"""
+    expected_headings = {
+        1: "Heading 1 - existing",
+        2: "Heading 2 - new",
+        3: None,
+        4: None,
+        5: None,
+        6: None,
+    }
+    expected_headings_for_prepend = {
+        1: "Heading 1 - existing",
+        2: "Heading 2 - existing",
+        3: None,
+        4: None,
+        5: None,
+        6: None,
+    }
+    expected_min_level = 3
+    result = markdown_splitter._get_all_headings(text, existing_headings)
+    print(result)
+    assert result == (
+        expected_headings,
+        expected_headings_for_prepend,
+        expected_min_level,
+    )
