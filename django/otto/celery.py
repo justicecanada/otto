c74b525f515a3821781a8e8cf9a03fc809a48f90
juleskuehn <jules.kuehn@justice.gc.ca>
2024-11-05 12:59:53 +0000

    add comment

--- django/otto/celery.py
+++ django/otto/celery.py
@@ -43,6 +43,7 @@ app.conf.beat_schedule = {
         "schedule": crontab(hour=2, minute=0),
     },
     # Delete unused libraries every day at 3 am UTC
+    # Actually let's NOT do this yet. Too risky. Needs clearer definition of "unused"
     # "delete-unused-libraries-every-morning": {
     #     "task": "otto.tasks.delete_unused_libraries",
     #     "schedule": crontab(hour=3, minute=0),
