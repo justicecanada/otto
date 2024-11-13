09eeb27126d2084aca8601bbe3dc8f1896977a11
jannable <jason@magnara.ca>
2024-11-13 13:51:49 -0500

    reverted entrypoint changes

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
-if [ "$1" = "backup" ]; then
+# Wait for the database to be ready
+echo "Waiting for database..."
+{ python manage.py wait_for_db || { echo "Error: Wait for database failed"; exit 1; } }
 
-    # Backup the databases
-    /django/backup_db.sh
+# Run migrations
+echo "Running migrations..."
+{ python manage.py migrate --noinput || { echo "Error: Migrate failed"; exit 1; } }
 
-else
+# Collect static files
+echo "Collecting static files..."
+{ python manage.py collectstatic --noinput --clear || { echo "Error: Collect static files failed"; exit 1; } }
 
-    # Wait for the database to be ready
-    echo "Waiting for database..."
-    { python manage.py wait_for_db || { echo "Error: Wait for database failed"; exit 1; } }
-
-    # Run migrations
-    echo "Running migrations..."
-    { python manage.py migrate --noinput || { echo "Error: Migrate failed"; exit 1; } }
-
-    # Collect static files
-    echo "Collecting static files..."
-    { python manage.py collectstatic --noinput --clear || { echo "Error: Collect static files failed"; exit 1; } }
-
-    # Start the application
-    daphne --bind 0.0.0.0 --port 8000 otto.asgi:application
-    
-fi
+exec "$@"
