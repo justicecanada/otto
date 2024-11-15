ff3a5095d427d3eaa9f104d9a8df4a1bca151e3d
smcdonald-jus <shaun.mcdonald@justice.gc.ca>
2024-11-15 22:22:06 +0000

    Fix CAD cost display

--- django/otto/utils/common.py
+++ django/otto/utils/common.py
@@ -13,10 +13,11 @@ def file_size_to_string(filesize):
 
 
 def display_cad_cost(usd_cost):
+    from otto.models import OttoStatus # Need to do it here to avoid circular imports
     """
     Converts a USD cost to CAD and returns a formatted string
     """
-    approx_cost_cad = float(usd_cost) * settings.USD_TO_CAD
+    approx_cost_cad = float(usd_cost) * OttoStatus.objects.singleton().exchange_rate
     if approx_cost_cad < 0.01:
         return "< $0.01"
     return f"${approx_cost_cad:.2f}"
