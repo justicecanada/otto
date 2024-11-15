2065c27bddc9c2983669597885f785d4b6fcc01a
juleskuehn <jules.kuehn@justice.gc.ca>
2024-11-15 16:02:34 +0000

    text extractor: remove 'download all' button

--- django/text_extractor/views.py
+++ django/text_extractor/views.py
@@ -163,8 +163,10 @@ def poll_tasks(request, user_request_id):
         context.update(
             {"poll_url": reverse("text_extractor:poll_tasks", args=[user_request.id])}
         )
-    else:
-        context.update({"show_download_all_button": True})
+    # else:
+    #     "Download all" doesn't work in prod. Disabling for now.
+    #     TODO: Zipped download of all files
+    #     context.update({"show_download_all_button": True})
 
     # In an HTMX request, we just want the updated rows.
     if request.headers.get("HX-Request") == "true":
