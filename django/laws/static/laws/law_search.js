5228d87558f095d767c6c3c451225293f0e8e8da
juleskuehn <jules.kuehn@justice.gc.ca>
2024-11-19 22:09:10 +0000

    wip: use markdown-it in laws app

--- django/laws/static/laws/law_search.js
+++ django/laws/static/laws/law_search.js
@@ -111,3 +111,36 @@ function findSimilar(el) {
   // Submit the form
   document.getElementById('basic-search-button').click();
 }
+
+const md = markdownit({
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return '<pre><code class="hljs">' +
+          hljs.highlight(str, {language: lang, ignoreIllegals: true}).value +
+          '</code></pre>';
+      } catch (__) { }
+    }
+
+    return '<pre><code class="hljs">' + md.utils.escapeHtml(str) + '</code></pre>';
+  }
+});
+
+md.use(katexPlugin);
+
+function render_markdown(element) {
+  // Render markdown in the element
+  const markdown_text = element.querySelector(".markdown-text");
+  if (markdown_text) {
+    let to_parse = markdown_text.dataset.md;
+    try {
+      to_parse = JSON.parse(to_parse);
+    } catch (e) {
+      to_parse = false;
+    }
+    if (to_parse) {
+      const parent = markdown_text.parentElement;
+      parent.innerHTML = md.render(to_parse);
+    }
+  }
+}
