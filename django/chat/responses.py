46cb0825835d94e232e61d437c6fb4668b1219b4
Jules Kuehn <jk@jules.lol>
2024-11-18 21:05:20 -0500

    fix: error handling/reporting (chat, librarian) (#124)
    * fix: prevent infinite retries on otto_response error
    * detailed error message in from chat and librarian in logs with matching Error ID displayed to user
    * Only show Error ID (not full message) if DEBUG is False
    * save full trace from chat response
    * test chat message error
    * fix: QA summarize mode on entire library

--- django/chat/responses.py
+++ django/chat/responses.py
@@ -1,6 +1,8 @@
 import asyncio
-from itertools import groupby
+import traceback
+import uuid
 
+from django.conf import settings
 from django.core.cache import cache
 from django.core.exceptions import ValidationError
 from django.core.validators import URLValidator
@@ -43,26 +45,29 @@ def otto_response(request, message_id=None, switch_mode=False, skip_agent=False)
     Stream a response to the user's message. Uses LlamaIndex to manage chat history.
     """
     response_message = Message.objects.get(id=message_id)
-    chat = response_message.chat
-    mode = chat.options.mode
+    try:
+        chat = response_message.chat
+        mode = chat.options.mode
 
-    # For costing and logging. Contextvars are accessible anytime during the request
-    # including in async functions (i.e. htmx_stream) and Celery tasks.
-    bind_contextvars(message_id=message_id, feature=mode)
+        # For costing and logging. Contextvars are accessible anytime during the request
+        # including in async functions (i.e. htmx_stream) and Celery tasks.
+        bind_contextvars(message_id=message_id, feature=mode)
 
-    agent_enabled = not skip_agent and mode == "chat" and chat.options.chat_agent
-    if agent_enabled:
-        return chat_agent(chat, response_message)
-    if mode == "chat":
-        return chat_response(chat, response_message, switch_mode=switch_mode)
-    if mode == "summarize":
-        return summarize_response(chat, response_message)
-    if mode == "translate":
-        return translate_response(chat, response_message)
-    if mode == "qa":
-        return qa_response(chat, response_message, switch_mode=switch_mode)
-    else:
-        return error_response(chat, response_message)
+        agent_enabled = not skip_agent and mode == "chat" and chat.options.chat_agent
+        if agent_enabled:
+            return chat_agent(chat, response_message)
+        if mode == "chat":
+            return chat_response(chat, response_message, switch_mode=switch_mode)
+        if mode == "summarize":
+            return summarize_response(chat, response_message)
+        if mode == "translate":
+            return translate_response(chat, response_message)
+        if mode == "qa":
+            return qa_response(chat, response_message, switch_mode=switch_mode)
+        else:
+            return error_response(chat, response_message, _("Invalid mode."))
+    except Exception as e:
+        return error_response(chat, response_message, e)
 
 
 def chat_response(
@@ -393,6 +398,7 @@ def qa_response(chat, response_message, switch_mode=False):
 
     # Apply filters if we are in qa mode and specific data sources are selected
     qa_scope = chat.options.qa_scope
+    filter_documents = None
     if qa_scope == "data_sources":
         data_sources = chat.options.qa_data_sources.all()
         filter_documents = Document.objects.filter(data_source__in=data_sources)
@@ -416,7 +422,11 @@ def qa_response(chat, response_message, switch_mode=False):
 
     # Summarize mode
     if chat.options.qa_mode == "summarize":
-        # Use summarization on each of the filter_documents
+        # Use summarization on each of the documents
+        if not filter_documents:
+            filter_documents = Document.objects.filter(
+                data_source__library=chat.options.qa_library
+            )
         document_titles = [document.name for document in filter_documents]
         summary_responses = [
             llm.tree_summarize(
@@ -575,17 +585,30 @@ def qa_response(chat, response_message, switch_mode=False):
     )
 
 
-def error_response(chat, response_message):
+def error_response(chat, response_message, error_message=None):
     """
     Send an error message to the user.
     """
     llm = OttoLLM()
+    response_str = _("There was an error processing your request.")
+    error_id = str(uuid.uuid4())[:7]
+
+    if error_message and settings.DEBUG:
+        response_str += f"\n\n```\n{error_message}\n```\n\n"
+    response_str += f" _({_('Error ID')}: {error_id})_"
+    logger.error(
+        "Error processing chat response",
+        error_id=error_id,
+        message_id=response_message.id,
+        chat_id=chat.id,
+        error=traceback.format_exc(),
+    )
     return StreamingHttpResponse(
         streaming_content=htmx_stream(
             chat,
             response_message.id,
             llm,
-            response_str=_("Sorry, this isn't working right now."),
+            response_str=response_str,
         ),
         content_type="text/event-stream",
     )
@@ -598,7 +621,7 @@ def chat_agent(chat, response_message):
     bind_contextvars(feature="chat_agent")
     user_message = response_message.parent
     if user_message is None:
-        return error_response(chat, response_message)
+        return error_response(chat, response_message, _("No user message found."))
 
     user_text = user_message.text
     if len(user_text) > 500:
