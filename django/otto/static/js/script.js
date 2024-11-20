93f766560e3e83feb12e3ddb7d8af75f586877ca
haitmo <127764435+haitmo@users.noreply.github.com>
2024-11-20 22:44:41 +0000

    styling and scripts adjustments.

--- django/otto/static/js/script.js
+++ django/otto/static/js/script.js
@@ -5,22 +5,11 @@ document.addEventListener('htmx:afterRequest', function (event) {
   }
 });
 
-
-
-; (function () {
-  const toastOptions = {delay: 2000, autohide: false};
-
+(function () {
+  const toastOptions = {delay: 2000};
   htmx.onLoad(() => {
     htmx.findAll(".toast").forEach((element) => {
       let toast = bootstrap.Toast.getInstance(element);
-
-      // Remove hidden toasts (optional)
-      if (toast && !toast.isShown()) {
-        toast.dispose();
-        element.remove();
-      }
-
-      // Show new ones
       if (!toast) {
         const toast = new bootstrap.Toast(element, toastOptions);
         toast.show();
