a51f10d9f5f50ffe809fd27e881e2280a5245c36
juleskuehn <jules.kuehn@justice.gc.ca>
2024-11-21 21:02:50 +0000

    add new files

---
+++ django/tests/chat/test_chat_readonly.py
@@ -0,0 +1,53 @@
+from django.urls import reverse
+
+import pytest
+
+from chat.models import Chat, Message
+
+
+@pytest.mark.django_db
+def test_chat(client, basic_user, all_apps_user):
+    # Create a chat with all apps user
+    # path("chat-with-ai/", views.new_chat_with_ai, name="chat_with_ai"),
+    user = all_apps_user()
+    client.force_login(user)
+    response = client.post(reverse("chat:chat_with_ai"))
+    # This should redirect to the chat page
+    assert response.status_code == 302
+    chat = Chat.objects.first()
+    assert chat.user == user
+
+    # Post a message to the chat
+    # path("id/<str:chat_id>/message/", views.chat_message, name="chat_message"),
+    response = client.post(
+        reverse("chat:chat_message", kwargs={"chat_id": chat.id}),
+        {"user-message": "Hello, world!"},
+    )
+    assert response.status_code == 200
+    # Check that the message was added to the chat
+    chat.refresh_from_db()
+    assert chat.messages.count() == 2  # 1 user message, 1 empty bot response
+    message = chat.messages.filter(is_bot=False).first()
+    assert message.text == "Hello, world!"
+
+    # Get the chat via the normal chat route
+    # path("id/<str:chat_id>/", views.chat, name="chat"),
+    response = client.get(reverse("chat:chat", kwargs={"chat_id": chat.id}))
+    assert response.status_code == 200
+    assert str(chat.id) in str(response.url)
+    assert "Hello, world!" in response.content.decode()
+    # Also check that there is a chat prompt (not readonly)
+    assert "chat-prompt" in response.content.decode()
+    assert "read only" not in response.content.decode().lower()
+
+    # Now, login as a different user
+    user = basic_user()
+    client.force_login(user)
+    # Get the chat
+    response = client.get(reverse("chat:chat", kwargs={"chat_id": chat.id}))
+    assert response.status_code == 200
+    assert str(chat.id) in str(response.url)
+    assert "Hello, world!" in response.content.decode()
+    # Check that there is no chat prompt (readonly)
+    assert "chat-prompt" not in response.content.decode()
+    assert "read only" in response.content.decode().lower()
