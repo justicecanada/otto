eebba99e43919157fb5d79c3fe141ed4f7f9edd2
Rifahaziz <rifah.aziz@justice.gc.ca>
2024-11-25 16:47:55 +0000

    changed appconfig class name to avoid duplication

--- django/lex_experiment/apps.py
+++ django/lex_experiment/apps.py
@@ -4,7 +4,7 @@ from django.apps import AppConfig
 from django.conf import settings
 
 
-class OcrConfig(AppConfig):
+class LexOcrConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "lex_experiment"
 
@@ -14,7 +14,7 @@ class OcrConfig(AppConfig):
         from django.core.files.storage import default_storage as storage
 
         # Define the path for the ocr_output_files directory
-        ocr_output_path = os.path.join(settings.MEDIA_ROOT, "ocr_output_files")
+        ocr_output_path = os.path.join(settings.MEDIA_ROOT, "lex_output_files")
 
         # Check if the directory exists, and create it if it doesn't
         if not storage.exists(ocr_output_path):
