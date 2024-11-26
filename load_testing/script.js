5f8846327bbaac17a7a4427ca5cae9d0ff462437
Jules Kuehn <jk@jules.lol>
2024-11-26 19:04:10 +0000

    display costs in dashboard; update tests script

--- load_testing/script.js
+++ load_testing/script.js
@@ -16,10 +16,14 @@ export const options = {
 export default function () {
   const base_url = 'http://localhost:8000';
   let res = http.get(`${base_url}/load_test/`); // Basic request
-  // let res = http.get(`${base_url}/load_test/?user_library_permissions`); // Heavy Django DB queries
+  // let res = http.get(`${base_url}/load_test/?user_library_permissions`); // Heavy Django DB query (takes 40s on local)
   // let res = http.get(`${base_url}/load_test/?sleep=10`); // Sleep for 10 seconds. Can be combined with other parameters.
   // let res = http.get(`${base_url}/load_test/?error`); // Raise an error in Django (returns 500)
   // let res = http.get(`${base_url}/load_test/?query_laws`); // Large vector DB query
+  // let res = http.get(`${base_url}/load_test/?llm_call`); // LLM call
+  // let res = http.get(`${base_url}/load_test/?llm_call=gpt-4o`); // Specific LLM deployment
+  // let res = http.get(`${base_url}/load_test/?embed_text`); // Embedding request
+  // let res = http.get(`${base_url}/load_test/?celery_sleep=30`); // Add mock Celery task and see queue lengths
   check(res, {
     'status is 200': (r) => r.status === 200,
   });
