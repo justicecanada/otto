9aa55ada21801db282903a506826590508d86ce2
Jules Kuehn <jk@jules.lol>
2024-11-26 21:36:07 +0000

    update k6 script

--- load_testing/k6_script.js
+++ load_testing/k6_script.js
@@ -1,11 +1,10 @@
 import {check, sleep} from 'k6';
-import http from 'k6/http';
 
 export const options = {
   // A number specifying the number of VUs to run concurrently.
   vus: 100,
   // A string specifying the total duration of the test run.
-  duration: '30s',
+  duration: '10s',
 };
 
 // The function that defines VU logic.
@@ -14,18 +13,19 @@ export const options = {
 // about authoring k6 scripts.
 //
 export default function () {
-  const base_url = 'https://otto-sandbox.canadacentral.cloudapp.azure.com';
+  const base_url = 'http://localhost:8000';
   // let res = http.get(`${base_url}/load_test/`); // Basic request
   // let res = http.get(`${base_url}/load_test/?user_library_permissions`); // Simple Django DB query
   // let res = http.get(`${base_url}/load_test/?user_library_permissions&heavy`); // Heavy Django DB query (takes 40s on local)
   // let res = http.get(`${base_url}/load_test/?sleep=10`); // Sleep for 10 seconds. Can be combined with other parameters.
-  // let res = http.get(`${base_url}/load_test/?sleep=10&show_queue`); // Returns the queue lengths in the response
+  // let res = http.get(`${base_url}/load_test/?celery_sleep=10`); // Adds a mock celery task that just sleeps for 10 seconds
+  // let res = http.get(`${base_url}/load_test/?celery_sleep=10&show_queue`); // Returns the task queue lengths in the response
   // let res = http.get(`${base_url}/load_test/?error`); // Raise an error in Django (returns 500)
   // let res = http.get(`${base_url}/load_test/?query_laws`); // Large vector DB query
   // let res = http.get(`${base_url}/load_test/?llm_call`); // LLM call
   // let res = http.get(`${base_url}/load_test/?llm_call=gpt-4o`); // Specific LLM deployment
   // let res = http.get(`${base_url}/load_test/?embed_text`); // Embedding request
-  let res = http.get(`${base_url}/load_test/?celery_sleep=5`); // Add mock Celery task and see queue lengths
+  // let res = http.get(`${base_url}/load_test/?celery_sleep=5`); // Add mock Celery task and see queue lengths
   check(res, {
     'status is 200': (r) => r.status === 200,
   });
