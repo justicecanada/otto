f9362d0a27d7b1a85515d8a127e932760dd8b901
Jules Kuehn <jk@jules.lol>
2024-11-29 19:11:05 +0000

    add retry to django db wrapper

--- django/postgres_wrapper/base.py
+++ django/postgres_wrapper/base.py
@@ -6,5 +6,9 @@ from retrying import retry
 
 
 class DatabaseWrapper(base.DatabaseWrapper):
+    @retry(
+        wait_exponential_multiplier=1000,
+        wait_exponential_max=20000,
+    )
     def get_new_connection(self, conn_params):
         return super().get_new_connection(conn_params)
