e61c29270187524be31a660c68a774fd23e3a02b
jannable <jason@magnara.ca>
2024-12-04 16:50:00 -0500

    enabled registration for aks private dns link in vnet where its deployed

--- setup/terraform/modules/aks/main.tf
+++ setup/terraform/modules/aks/main.tf
@@ -30,7 +30,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "aks_dns_link" {
   resource_group_name   = var.resource_group_name
   private_dns_zone_name = azurerm_private_dns_zone.aks_dns.name
   virtual_network_id    = var.vnet_id
-  registration_enabled  = false
+  registration_enabled  = true
 }
 
 # NSG for the AKS subnet to allow Inbound on port 443
