212b632f321989001a6e6a9855c9286dbecb61f3
jannable <jason@magnara.ca>
2024-12-12 14:28:24 -0500

    simplify your role assignments by using a single identity for keyvault access

--- setup/terraform/modules/aks/main.tf
+++ setup/terraform/modules/aks/main.tf
@@ -391,24 +391,21 @@ resource "azurerm_role_assignment" "rbac_cluster_admin" {
   scope                = azurerm_kubernetes_cluster.aks.id
 }
 
-# Role assignment for the AKS kubelet identity
+# Role assignment for the AKS secrets provider identity
 resource "azurerm_role_assignment" "aks_kubelet_identity_kv_secrets_user" {
   # SC-12: RBAC for AKS to access Key Vault secrets
-  #principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   principal_id         = data.azurerm_user_assigned_identity.identity.principal_id
   role_definition_name = "Key Vault Secrets User"
   scope                = var.keyvault_id
   principal_type       = "ServicePrincipal"
+
+  lifecycle {
+    ignore_changes = [
+      principal_id
+    ]
+  }
 }
 
-# Role assignment for the AKS secrets provider identity
-resource "azurerm_role_assignment" "aks_secrets_provider_identity_kv_secrets_user" {
-  # SC-12: RBAC for AKS to access Key Vault secrets
-  principal_id         = azurerm_kubernetes_cluster.aks.key_vault_secrets_provider[0].secret_identity[0].object_id
-  role_definition_name = "Key Vault Secrets User"
-  scope                = var.keyvault_id
-  principal_type       = "ServicePrincipal"
-}
 
 # ## Kubelet Identity
 # The kubelet identity is crucial for node-level operations, including accessing Azure resources like Key Vault. It's essential for pods that need to directly access secrets.
@@ -434,6 +431,13 @@ resource "azurerm_role_assignment" "aks_kubelet_identity_operator" {
 
   depends_on = [azurerm_kubernetes_cluster.aks]
 }
+
+resource "azurerm_role_assignment" "aks_managed_identity_operator" {
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  role_definition_name = "Managed Identity Operator"
+  scope                = data.azurerm_user_assigned_identity.identity.id
+}
+
 
 # This role assignment grants the AKS kubelet identity access to blob storage
 resource "azurerm_role_assignment" "aks_storage_blob_data_contributor" {
