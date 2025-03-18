d4fe315e4ab8ed0ef971a020cc1e7e028474d4e6
mcusteau <michel.custeau@justice.gc.ca>
2025-03-18 19:30:54 +0000

    more styling; text expansion

--- django/chat/static/chat/js/card.js
+++ django/chat/static/chat/js/card.js
@@ -5,6 +5,21 @@ var hasOverflow = (element) => {
 var elements = document.querySelectorAll('.preset-description');
 elements.forEach((element) => {
     if (hasOverflow(element)) {
-        element.setAttribute('title', element.innerText);
+        element.addEventListener('mouseenter', () => {
+            element.style.height = element.scrollHeight + 'px'; // Set height to full content height
+            element.style.position = 'absolute';
+            element.style.backgroundColor = 'rgb(255, 255, 255)';
+            element.style.width = '89%';
+            element.style.border = '1px solid #e0e0e0';
+
+        });
+        element.addEventListener('mouseleave', () => {
+            element.style.height = ''; // Reset height on mouse leave
+            element.style.position = ''; // Reset position
+            element.style.backgroundColor = ''; // Reset background color
+            element.style.width = ''; // Reset width
+            element.style.border = ''; // Reset border
+
+        });
     }
 });
