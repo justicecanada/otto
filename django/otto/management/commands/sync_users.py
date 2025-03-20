1c74984647493d2eabc296c5961cba3a3be5428c
haitmo <127764435+haitmo@users.noreply.github.com>
2025-03-20 16:55:15 +0000

    fix: adjusted user synchronization to correct failure in k8s recurring tasks

--- django/otto/management/commands/sync_users.py
+++ django/otto/management/commands/sync_users.py
@@ -5,7 +5,7 @@ from django.core.management.base import BaseCommand
 
 from django_extensions.management.utils import signalcommand
 
-from otto.utils.entra import sync_users_with_entra_async
+from otto.utils.entra import sync_users_with_entra
 
 
 class Command(BaseCommand):
@@ -13,14 +13,5 @@ class Command(BaseCommand):
 
     @signalcommand
     def handle(self, *args, **options):
-        try:
-            loop = asyncio.get_event_loop()
-        except RuntimeError as e:
-            if str(e).startswith("There is no current event loop"):
-                loop = asyncio.new_event_loop()
-                asyncio.set_event_loop(loop)
-            else:
-                raise
-        loop.run_until_complete(sync_users_with_entra_async())
-        loop.close()
+        sync_users_with_entra()
         self.stdout.write(self.style.SUCCESS("Users sync completed successfully."))
