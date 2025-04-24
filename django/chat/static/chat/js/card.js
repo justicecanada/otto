93aaba4131c876045e918279d3d302d2aeff9b44
Jules Kuehn <jk@jules.lol>
2025-04-24 22:59:10 +0000

    restyle preset cards with more text

--- django/chat/static/chat/js/card.js
+++ django/chat/static/chat/js/card.js
@@ -1,10 +1,28 @@
-var hasOverflow = (element) => {
-    return element.scrollHeight > element.clientHeight;
-};
+(function () {
+    const hasOverflow = (element) => {
+        return element.scrollHeight > element.clientHeight;
+    };
 
-var elements = document.querySelectorAll('.preset-description');
-elements.forEach((element) => {
-    if (hasOverflow(element)) {
-        element.setAttribute('title', element.innerText);
-    }
-});
+    const elements = document.querySelectorAll('#preset-card-list .preset-description');
+    elements.forEach((element) => {
+        if (hasOverflow(element)) {
+            element.setAttribute('title', element.innerText);
+        }
+    });
+
+    const cards = document.querySelectorAll('#preset-card-list li.card');
+    cards.forEach((card) => {
+        // When card is clicked (except for buttons and links),
+        // trigger click event on child a.preset-load-link
+        card.addEventListener('click', (event) => {
+            const target = event.target;
+            // Ensure target is not a button or link, or WITHIN a button or link
+            if (target.tagName !== 'A' && target.tagName !== 'BUTTON' && !target.closest('a') && !target.closest('button')) {
+                const link = card.querySelector('a.preset-load-link');
+                if (link) {
+                    link.click();
+                }
+            }
+        });
+    });
+})();
