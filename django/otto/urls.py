14177c1b72215138a7ae4ec5a2da40251c937053
amusbag <aliyu.musabago@justice.gc.ca>
2025-05-01 13:53:03 +0000

    add frequently asked questions view, add questions and answer, cleanup

--- django/otto/urls.py
+++ django/otto/urls.py
@@ -57,7 +57,11 @@ urlpatterns = [
     path("user_cost/", views.user_cost, name="user_cost"),
     path("extend_session/", views.extend_session, name="extend_session"),
     path("terms_of_use/", views.terms_of_use, name="terms_of_use"),
-    path("faqs/", views.faqs, name="faqs"),
+    path(
+        "frequently_asked_questions/",
+        views.frequently_asked_questions,
+        name="frequently_asked_questions",
+    ),
     path(
         "user_management/mark_tour_completed/<str:tour_name>/",
         views.mark_tour_completed,
