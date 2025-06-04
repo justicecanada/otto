df2dc78dde7040ee7370605d671c2d29d5dc356a
Jules Kuehn <jk@jules.lol>
2025-06-04 16:43:36 -0400

    wip: file upload

--- django/template_wizard/static/template_wizard/scripts.js
+++ django/template_wizard/static/template_wizard/scripts.js
@@ -24,3 +24,97 @@ cards.forEach((card) => {
     }
   });
 });
+
+function initTemplateWizardUploadForm() {
+  const metadata = {};
+  const upload_message = document.getElementById("template-wizard-upload-message");
+  const details_container = document.getElementById("template-wizard-details");
+  const file_input = document.getElementById("id_template-wizard-input_file");
+  function submitUploadsIfComplete() {
+    const form = document.getElementById("template-wizard-upload-form");
+    const allDone = Array.from(form.querySelectorAll(".dff-file")).every(file => {
+      return file.classList.contains("dff-upload-success") || file.classList.contains("dff-upload-fail");
+    });
+    if (allDone) {
+      htmx.trigger(form, 'htmx:beforeRequest');
+      htmx.ajax('POST', form.getAttribute('hx-post'), {
+        source: form,
+        target: form.getAttribute('hx-target'),
+        swap: form.getAttribute('hx-swap'),
+      });
+    }
+    hideIfNoFiles();
+  }
+  function hideIfNoFiles() {
+    const form = document.getElementById("template-wizard-upload-form");
+    const numFiles = form.querySelectorAll(".dff-file").length;
+    if (numFiles === 0) {
+      upload_message.classList.add("d-none");
+      details_container.classList.remove("d-none");
+    } else {
+      upload_message.classList.remove("d-none");
+      details_container.classList.add("d-none");
+    }
+  }
+  if (file_input) {
+    initUploadFields(document.getElementById("template-wizard-upload-form"), {
+      prefix: "template-wizard",
+      supportDropArea: false,
+      callbacks: {
+        onSuccess: (upload) => {
+          const filename = upload.name;
+          const contentType = upload.upload.file.type;
+          metadata[filename] = {type: contentType};
+          const metadataField = document.querySelector("#id_template-wizard-input_file-metadata");
+          if (metadataField) {
+            metadataField.value = JSON.stringify(metadata);
+          }
+          submitUploadsIfComplete();
+        },
+        onError: (upload) => submitUploadsIfComplete(),
+        onDelete: (upload) => submitUploadsIfComplete(),
+        onProgress: (bytesUploaded, bytesTotal, upload) => {
+          if (typeof TEMPLATE_WIZARD_MAX_UPLOAD_SIZE !== 'undefined' && bytesTotal > TEMPLATE_WIZARD_MAX_UPLOAD_SIZE) {
+            const fileElements = document.querySelectorAll('#template-wizard-upload-form .dff-file');
+            const fileElement = Array.from(fileElements).find((fileElement) => {
+              const filenameElement = fileElement.querySelector('.dff-filename');
+              return filenameElement && filenameElement.innerHTML === upload.name;
+            });
+            if (fileElement) {
+              const cancel_link = fileElement.querySelector('a.dff-cancel');
+              const progress_bar = fileElement.querySelector('.dff-progress');
+              const error_message = fileElement.querySelector('.dff-error');
+              if (cancel_link) {
+                cancel_link.remove();
+              }
+              if (progress_bar) {
+                progress_bar.remove();
+              }
+              if (!error_message) {
+                const error_message = document.createElement('span');
+                error_message.className = 'dff-error';
+                error_message.innerText = TEMPLATE_WIZARD_UPLOAD_TOO_LARGE;
+                fileElement.appendChild(error_message);
+              }
+            }
+            upload.abort(true);
+            setTimeout(() => {
+              fileElement.classList.add("dff-upload-fail");
+              submitUploadsIfComplete();
+            }, 2000);
+          }
+        },
+      }
+    });
+    file_input.addEventListener("change", function () {
+      upload_message.classList.remove("d-none");
+      details_container.classList.add("d-none");
+    });
+  }
+}
+document.addEventListener('htmx:afterSwap', function (event) {
+  if (event.target.id === "template-wizard-upload-message") {
+    initTemplateWizardUploadForm();
+  }
+});
+initTemplateWizardUploadForm();
--- django/template_wizard/views/main.py
+++ django/template_wizard/views/main.py
@@ -5,6 +5,8 @@ from django.utils.translation import gettext as _
 
 from rules.contrib.views import objectgetter
 
+from chat.forms import UploadForm
+from librarian.models import SavedFile
 from otto.utils.decorators import app_access_required, permission_required
 from template_wizard.forms import FieldForm, LayoutForm, MetadataForm, SourceForm
 from template_wizard.models import Template, TemplateField
@@ -44,11 +46,28 @@ def fill_template(request, template_id):
 )
 def select_sources(request, template_id):
     template = get_object_or_404(Template, id=template_id)
+    upload_form = UploadForm(prefix="template-wizard")
+    upload_success = False
+    saved_files = []
+    if request.method == "POST":
+        upload_form = UploadForm(request.POST, request.FILES, prefix="template-wizard")
+        if upload_form.is_valid():
+            saved_files = upload_form.save()
+            upload_success = True
+            messages.success(
+                request, _(f"{len(saved_files)} file(s) uploaded successfully.")
+            )
+            upload_form = UploadForm(
+                prefix="template_wizard"
+            )  # Reset form after upload
     return render(
         request,
         "template_wizard/use_template/select_sources.html",
         context={
             "hide_breadcrumbs": True,
             "template": template,
+            "upload_form": upload_form,
+            "upload_success": upload_success,
+            "saved_files": saved_files,
         },
     )
