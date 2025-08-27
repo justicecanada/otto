bc932452453e439fdde03eaac1b07afe5351a065
smcdonald-jus <shaun.mcdonald@justice.gc.ca>
2025-08-27 16:21:48 +0000

    fix permissions for doc processing polling

--- django/librarian/views.py
+++ django/librarian/views.py
@@ -316,7 +316,7 @@ def modal_view(request, item_type=None, item_id=None, parent_id=None):
 
 
 @permission_required(
-    "librarian.edit_data_source", objectgetter(DataSource, "data_source_id")
+    "librarian.view_data_source", objectgetter(DataSource, "data_source_id")
 )
 def poll_status(request, data_source_id, document_id=None):
     """
